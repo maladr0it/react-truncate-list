94909914f793c29df8559dee9f7f4aaef3704df3
lgbradford <lgbradford@indeed.com>
2020-02-12 15:11:15 +0900

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import replace from "@rollup/plugin-replace";
+import babel from "rollup-plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+const NODE_ENV = process.env.NODE_ENV || "development";
+const outputPath =
+  NODE_ENV === "production" ? "package/prod.js" : "package/dev.js";
+
+export default {
+  input: "src/index.tsx",
+  output: {
+    file: outputPath,
+    format: "cjs",
+  },
+  external: ["react", "react-dom"],
+  plugins: [
+    replace({ "process.env.NODE_ENV": JSON.stringify(NODE_ENV) }),
+    babel({ exclude: "node_modules/**", extensions: [".js", ".ts", ".tsx"] }),
+    resolve(),
+    commonjs({ include: "node_modules/**" }),
+  ],
+};
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Component = () => {
+  return <h1>Hello World</h1>;
+};
